c34af674a03011fb9cbffd917a86653d0dc76033
Juan Santana <juanka9755@hotmail.com>
2023-04-09 11:28:37 -0500

    03-COUNTER APP
    Se implementa componente de contador
    Se hace uso del hook useState
    Se implementa evento de click del boton
    Uso de props
    Uso de los propTypes y defaultProps

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -3,8 +3,40 @@
 //     return <h1>Hola Mundo</h1>;
 // }
 
-export const CounterApp = () => {
+import PropTypes from 'prop-types'
+import { useState } from 'react'
+
+export const CounterApp = ({ value }) => {
+
+  const [ counter, setCounter ] = useState( value );
+
+  const sum = () => {
+    // setCounter( counter + 1 );
+    setCounter ( (c) => c + 1);
+  }
+
+  const subtract = () => {
+
+    setCounter ( (c) => c - 1);
+  }
+
+  const reset = () => {
+    
+    setCounter ((c) => c = value);
+  }
+
   return (
-    <h1> CounterApp </h1>
+    <>
+      <h1> CounterApp </h1>
+      <h2> { counter } </h2>
+
+      <button onClick={ sum }> +1 </button>
+      <button onClick={ subtract }> -1 </button>
+      <button onClick={ reset }> Reset </button>
+    </>
   )
+}
+
+CounterApp.protoTypes ={
+  value: PropTypes.number.isRequired
 }
--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,15 +1,28 @@
+import PropTypes from 'prop-types'
+
 const name = 'Juan Camilo';
 
 const getName = ( name ) => {
-    return `Hola, soy ${name}`;
+    return `Hola, soy ${ name }`;
 }
 
-export const FirstApp = () => {
+export const FirstApp = ({ title, subtitle }) => {
 
   return (
     <>
-        <span> { getName('Juan') } </span>
-        <p>Primera aplicación en React</p>
+        {/* <span> { getName('Juan') } </span> */}
+        <span>{ title }</span>
+        <p>{ subtitle + 25 }</p>
     </>
   )
+}
+
+FirstApp.propTypes = {
+  title: PropTypes.string.isRequired,
+  subtitle: PropTypes.number
+}
+
+FirstApp.defaultProps = {
+  title: 'Titulo por defecto',
+  subtitle: 0
 }
